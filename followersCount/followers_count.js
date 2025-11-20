bdf422971f1f69320b5bcd90c4cec4a2375ed260
Sokpanha PRAK <praksokpanha05@gmail.com>
2025-11-19 22:19:11 -0500

    commit follower count

---
+++ followersCount/followers_count.js
@@ -0,0 +1,19 @@
+let count = 0; // Initialze count to 0
+
+function increaseCount() {
+    count++; // Increment the count by 1
+    displayCount(); // Display the count
+    checkCountValue(); // Check count value and display messages
+}
+
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML = count; // Display the count in the HTML
+}
+
+function checkCountValue() {
+    if (count === 10) {
+        alert("Your Instagram post gained 10 follwers! Congratulations!");
+    } else if (count === 20) {
+        alter("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
